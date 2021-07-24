4b631df85814bb23da75501e0d94b2edc9771c58
Disha Shah <dishashah2019@gmail.com>
2021-07-24 14:41:57 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.components.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51JGdNPSEZsfh6VB2Q6rAvbzL4OYDU0rUstVhW8aVRmLxm2CxQUfU4ln2QkwTdwWMVBnJESNU1q8VuQ2XrM4wIE3p00BaoWKFoL';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
